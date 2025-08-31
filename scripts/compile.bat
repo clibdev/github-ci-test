d006729c7008cb14efef157f682a6bd2d077475a
clibdev <clibdev@gmail.com>
2025-08-31 15:07:51 +0300

    fix workflow

--- scripts/compile.bat
+++ scripts/compile.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal
 
-cmake -B build -G Ninja -DCMAKE_BUILD_TYPE=Release \
-    -DBUILD_TESTING=OFF \
-    -DCMAKE_INSTALL_BINDIR='.' \
+cmake -B build -G Ninja -DCMAKE_BUILD_TYPE=Release^
+    -DBUILD_TESTING=OFF^
+    -DCMAKE_INSTALL_BINDIR='.'^
     -DCMAKE_INSTALL_PREFIX=/app/build
