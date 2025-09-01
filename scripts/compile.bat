5c3440144147dd34f9bb895439742e150124ce3f
clibdev <clibdev@gmail.com>
2025-09-01 11:46:44 +0300

    fix workflow

--- scripts/compile.bat
+++ scripts/compile.bat
@@ -4,7 +4,7 @@ setlocal
 git clone https://github.com/ninja-build/ninja.git --depth=1 --branch=v1.13.1
 cd ninja
 
-cmake -B build -G Ninja -DCMAKE_BUILD_TYPE=Release^
+cmake -B build -G Ninja -A "x64" -DCMAKE_BUILD_TYPE=Release^
     -DBUILD_TESTING=OFF^
     -DCMAKE_INSTALL_BINDIR='.'^
     -DCMAKE_INSTALL_PREFIX=.\build
