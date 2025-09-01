73eddf80451f55517664e19ea8ce5f3ef6b9e631
clibdev <clibdev@gmail.com>
2025-09-01 13:15:16 +0300

    fix workflow

--- scripts/compile.bat
+++ scripts/compile.bat
@@ -1,6 +1,10 @@
 @echo off
 setlocal
 
+for /f "tokens=2 delims=:," %%i in ('curl -s https://api.github.com/repos/ninja-build/ninja/releases/latest ^| findstr "tag_name"') do set VERSION=%%~i
+set VERSION=%VERSION:"=%
+echo %VERSION%
+
 set WORKDIR=%CD%
 
 cd %WORKDIR%
@@ -14,4 +18,4 @@ cmake -B build -G Ninja -DCMAKE_BUILD_TYPE=Release^
 cmake --build build -j%NUMBER_OF_PROCESSORS%
 cmake --install build --strip
 
-move %WORKDIR%\build\ninja.exe %WORKDIR%\build\ninja-x86_64-windows-msvc
+move %WORKDIR%\build\ninja.exe %WORKDIR%\build\ninja-x86_64-windows-msvc >nul
