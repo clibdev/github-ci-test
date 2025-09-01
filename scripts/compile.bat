33f00042db603aeabe51c316860e06f49ae0a8d0
clibdev <clibdev@gmail.com>
2025-09-01 13:00:31 +0300

    fix workflow

--- scripts/compile.bat
+++ scripts/compile.bat
@@ -8,5 +8,5 @@ cmake -B build -G Ninja -DCMAKE_BUILD_TYPE=Release^
     -DBUILD_TESTING=OFF^
     -DCMAKE_INSTALL_BINDIR='.'^
     -DCMAKE_INSTALL_PREFIX=.\build
-cmake --build build -j$(nproc)
+cmake --build build -j%NUMBER_OF_PROCESSORS%
 cmake --install build --strip
