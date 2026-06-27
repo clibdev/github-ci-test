68d7e641eaca62e1db00303cd1bcc17e7e7acbd3
clibdev <clibdev@gmail.com>
2026-06-27 13:54:50 +0300

    update scripts

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -2,26 +2,28 @@
 
 VERSION=$(curl -s https://api.github.com/repos/ninja-build/ninja/releases/latest | grep -Po '"tag_name": "\Kv[0-9.]+')
 
-cd /tmp
-git clone https://github.com/ninja-build/ninja.git --depth=1 --branch=$VERSION
-cd ninja
+git clone https://github.com/ninja-build/ninja.git --depth=1 --branch=$VERSION /tmp/ninja
+mkdir -p /app/build
 
 TOOLCHAINS=(
   'x86_64-linux-gnu'
   'aarch64-linux-gnu'
+  'arm-linux-gnueabi'
+  'riscv64-linux-gnu'
 )
 
 for toolchain in "${TOOLCHAINS[@]}"; do
   export CXX=/opt/$toolchain/bin/$toolchain-g++
 
+  cd /tmp/ninja
   rm -rf build
   cmake -B build -G Ninja -DCMAKE_BUILD_TYPE=Release \
-      -DBUILD_TESTING=OFF \
-      -DCMAKE_EXE_LINKER_FLAGS='-static-libstdc++ -static-libgcc' \
-      -DCMAKE_INSTALL_BINDIR='.' \
-      -DCMAKE_INSTALL_PREFIX=/app/build
+    -DCMAKE_INSTALL_PREFIX=build/install \
+    -DCMAKE_INSTALL_BINDIR='.' \
+    -DCMAKE_EXE_LINKER_FLAGS='-static-libstdc++ -static-libgcc' \
+    -DBUILD_TESTING=OFF
   cmake --build build -j$(nproc)
   cmake --install build --strip
 
-  mv /app/build/ninja /app/build/ninja-$toolchain
+  cd build/install && tar czf /app/build/ninja-$toolchain.tar.gz ninja
 done
