d3a5fc5a316ef48515889894106524ca10093632
clibdev <clibdev@gmail.com>
2026-06-28 10:43:46 +0300

    changes

--- scripts/compile.ps1
+++ scripts/compile.ps1
@@ -7,18 +7,17 @@ git clone https://github.com/ninja-build/ninja.git --depth=1 --branch=$VERSION
 mkdir -Force $WORKDIR\build | Out-Null
 
 $TOOLCHAINS = @(
-  @{ Name = 'x86_64-windows-msvc';  VCVars = "$($args[0])\vcvars64.bat" }
-  @{ Name = 'aarch64-windows-msvc'; VCVars = "$($args[0])\vcvarsamd64_arm64.bat" }
+  @('x86_64-windows-msvc', "$($args[0])\vcvars64.bat")
+  @('aarch64-windows-msvc', "$($args[0])\vcvarsamd64_arm64.bat")
 )
 
 foreach ($toolchain in $TOOLCHAINS) {
 & {
-  cmd /c "`"$($toolchain.VCVars)`" && set" | ? { $_ -match '=' } | % {
+  echo $VERSION
+  cmd /c "`"$($toolchain[1])`" && set" | ? { $_ -match '=' } | % {
     $name, $value = $_ -split '=', 2
     [System.Environment]::SetEnvironmentVariable($name, $value)
   }
-
-  echo $VERSION
 
   cd $WORKDIR\ninja && rm -r -Force build -ea SilentlyContinue
   cmake -B build -G Ninja -DCMAKE_BUILD_TYPE=Release `
@@ -28,6 +27,6 @@ foreach ($toolchain in $TOOLCHAINS) {
   cmake --build build -j $env:NUMBER_OF_PROCESSORS
   cmake --install build --strip
 
-  cd build\install && tar czf $WORKDIR\build\ninja-$($toolchain.Name).tar.gz ninja.exe
-} 2>&1 | tee $WORKDIR\build\ninja-$($toolchain.Name).txt
+  cd build\install && tar czf $WORKDIR\build\ninja-$($toolchain[0]).tar.gz ninja.exe
+} 2>&1 | tee $WORKDIR\build\ninja-$($toolchain[0]).txt
 }
