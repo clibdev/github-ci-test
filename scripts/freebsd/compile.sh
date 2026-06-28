6dfaa2dc718925880887533e296f3645997e12eb
clibdev <clibdev@gmail.com>
2026-06-28 13:04:09 +0300

    freebsd

---
+++ scripts/freebsd/compile.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+set -eo pipefail
+
+VERSION=$(curl -s https://api.github.com/repos/ninja-build/ninja/releases/latest | grep -oE '"tag_name": "v[0-9.]+"' | grep -oE 'v[0-9.]+')
+
+git clone https://github.com/ninja-build/ninja.git --depth=1 --branch=$VERSION /tmp/ninja
+mkdir -p build
+
+WORKDIR=$(pwd)
+
+{
+  echo $VERSION
+
+  cd /tmp/ninja && rm -rf build
+  cmake -B build -G Ninja -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX=build/install \
+    -DCMAKE_INSTALL_BINDIR='.' \
+    -DCMAKE_EXE_LINKER_FLAGS='-static' \
+    -DBUILD_TESTING=OFF
+  cmake --build build -j$(nproc)
+  cmake --install build --strip
+
+  cd build/install && tar czf $WORKDIR/build/ninja-x86_64-freebsd.tar.gz ninja
+} 2>&1 | tee $WORKDIR/build/ninja-x86_64-freebsd.txt
